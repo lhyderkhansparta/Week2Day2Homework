9408ad8b7b78dd80530f5248e3743de9bd88edd5
Liinakhan Hyderkhan <LHyderkhan@spartaglobal.com>
2019-10-29 17:25:02 +0000

    This is week 2 day 2 homework

---
+++ Week2Day2Homework/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Day2Homework
+{
+    class Hospital
+    {
+        public string Address;
+        public string Postcode;
+    }
+    class Employee : Hospital
+    {
+        public string Name;
+        public string Gender;
+        public string Age;
+        public string Job;
+    }
+    class Nurse : Employee
+    {
+        private int Numberofcompletedbloodtests;
+        private int Numberofpatientsonward;
+    }
+    class Patient : Hospital
+    {
+        public string Illness;
+        public string Name;
+        public int Age;
+        private string Gender;
+    }
+    class Newborn : Patient
+    {
+        private bool hasAllVaccines;
+        private bool isinincubator;
+    }
+    class Department : Hospital
+    {
+        private string Ward;
+        private int Numberofpatients;
+        public int NumberOfDoctors;
+        public int NumberOfRooms;
+    }
+    class Paedatric : Department
+    {
+        private int NumberOfChildren;
+        private bool HasPlayingArea;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
